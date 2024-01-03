5328bf1b58c6f8e871360816ebcf6c20592eb8cf
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2024-01-03 16:18:31 +0530

    loading state added

--- src/components/MealSection.jsx
+++ src/components/MealSection.jsx
@@ -3,29 +3,42 @@ import Meal from "./Meal";
 
 function MealSection() {
   const [mealsArray, setMealsArray] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      const data = await response.json();
-      setMealsArray(data);
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:3000/meals");
+
+        const data = await response.json();
+        setMealsArray(data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
     }
+
     fetchMeals();
   }, []);
   return (
     <div id="meals">
-      {mealsArray.map((item) => {
-        return (
-          <Meal
-            key={item.id}
-            id={item.id}
-            mealName={item.name}
-            mealImg={`http://localhost:3000/${item.image}`}
-            mealDescription={item.description}
-            mealPrice={item.price}
-          />
-        );
-      })}
+      {isLoading && <p>loading data...</p>}
+      {isError && <p>error fetching</p>}
+      {!isError &&
+        mealsArray.map((item) => {
+          return (
+            <Meal
+              key={item.id}
+              id={item.id}
+              mealName={item.name}
+              mealImg={`http://localhost:3000/${item.image}`}
+              mealDescription={item.description}
+              mealPrice={item.price}
+            />
+          );
+        })}
     </div>
   );
 }
